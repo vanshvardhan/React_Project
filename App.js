68f54fce7aa51689e265df010a155d2c5ff20fa4
vanshvardhan <vansh.vardhan55607@gmail.com>
2025-09-18 21:25:56 +0530

    Add README with rocket emoji and update project files

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", null, "I am h1 tag"),
+    React.createElement("h2", null, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", null, "I am h1 tag"),
+    React.createElement("h2", null, "I am h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+//const heading = React.createElement("h1", {id: "heading"}, "Namaste React from Javascript");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
